d803303241f0f64a3f1b45011e8ddfb4197464de
jisu <jisu3316@naver.com>
2022-05-10 20:44:45 +0900

    jpaMaven

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop;
+
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();//database 커넥션 하나 받았다 생각하면됨.
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+}
